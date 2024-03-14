e98e5f42d8a2c25b4c67796bbdcddcd9cd298dc0
George Luo <gluo@Georges-MacBook-Air.local>
2024-03-14 13:05:43 -0700

    add json stuff

--- pkg/promptbuilder/prompt_outline.go
+++ pkg/promptbuilder/prompt_outline.go
@@ -2,25 +2,25 @@ package promptbuilder
 
 // PromptOutline is used to display the components of a prompt
 type PromptOutline struct {
-	TopLevelRequirement string
-	OrgMetadata         OrgMetaData
-	Functions           []Function
-	Files               []FileMetadata
+	TopLevelRequirement string         `json:"top_level_requirement"`
+	OrgMetadata         OrgMetaData    `json:"org_metadata"`
+	Functions           []Function     `json:"functions"`
+	Files               []FileMetadata `json:"files"`
 }
 
 type OrgMetaData struct {
-	ID               string
-	Name             string
-	MaxDirectReports string
-	ReportingTo      []string
-	DirectReports    []string
+	ID               string   `json:"id"`
+	Name             string   `json:"name"`
+	MaxDirectReports string   `json:"max_direct_reports"`
+	ReportingTo      []string `json:"reporting_to"`
+	DirectReports    []string `json:"direct_reports"`
 }
 
 type FileMetadata struct {
-	Category string
-	Filename string
+	Category string `json:"category"`
+	Filename string `json:"filename"`
 }
 
 type Function struct {
-	Description string
+	Description string `json:"desc"`
 }
